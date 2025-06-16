7fbc0ae175dc3b952db6e9109b432a5b60f03493
NatanLomba <144914718+Piwler@users.noreply.github.com>
2025-06-16 08:27:17 -0300

    16/06

---
+++ main.js
@@ -0,0 +1,7 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSernha = 12;
+numeroSenha.textContent = tamanhoSernha;
+
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+
+console.log(botoes);
