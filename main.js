e02d426f6768069e12103c6fad6ac56d3d8726eb
NatanLomba <144914718+Piwler@users.noreply.github.com>
2025-06-16 09:04:16 -0300

    16/06 3

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const numeroSenha = document.querySelector('.parametro-senha_texto');
-let tamanhoSernha = 12;
-numeroSenha.textContent = tamanhoSernha;
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha_botao');
 
-console.log(botoes);
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho() {
+     if (tamanhoSenha < 20){   
+         tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha
+}
